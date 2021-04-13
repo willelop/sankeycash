3b1690724a63c86b0713e946cc57d1ec332ac74a
willelop <willelopz@gmail.com>
2021-04-13 23:13:39 +0200

    Improved compliance to pylint

--- GnucashInterface.py
+++ GnucashInterface.py
@@ -1,30 +1,33 @@
-
 from decimal import Decimal
 from math import log10
-from datetime import date, datetime, timedelta
-from gnucash import Session,Split, GncNumeric, Account, ACCT_TYPE_TRADING, ACCT_TYPE_EXPENSE, ACCT_TYPE_INCOME, SessionOpenMode 
+from datetime import datetime, timedelta
+from gnucash import Session, GncNumeric, ACCT_TYPE_TRADING,\
+    ACCT_TYPE_EXPENSE, ACCT_TYPE_INCOME, SessionOpenMode
 
 ZERO = Decimal(0)
+
 
 class GnucashInterface():
 
     """Documentation for ClassName
 
     """
-    def __init__(self,file,_income_acc,_expense_acc,_equity_acc):
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, file, _income_acc, _expense_acc, _equity_acc):
         self.deep_level = 1
         self.income_name = _income_acc
         self.expense_name = _expense_acc
         self.equity_name = _equity_acc
         self.session = Session(file, mode=SessionOpenMode.SESSION_READ_ONLY)
-        self.root = self.session.book.get_root_account()
         self.book = self.session.book
+        self.root = self.session.book.get_root_account()
         self.id_map = {}
         self.id_names = []
         self.id_names_short = []
         self.generate_ids()
         self.assess_datetime = datetime.today()
         self.assess_period = 'month'
+        print(self.root.GetCommodity())
 
     def generate_ids(self):
         cnt = 0
@@ -38,16 +41,15 @@ class GnucashInterface():
             self.id_names.append(test.get_full_name())
             self.id_names_short.append(test.GetName())
 
-    def get_account_names(self,use_short: bool):
+    def get_account_names(self, use_short: bool):
         if use_short:
             return self.id_names_short
-        else:
-            return self.id_names
+        return self.id_names
 
     def get_root_commodity(self):
-        return self.root.GetCommodity()
+        return self.root.lookup_by_name("Assets").GetCommodity()
 
-    def gnc_numeric_to_python_Decimal(self,numeric):
+    def gnc_numeric_to_python_decimal(self, numeric):
         negative = numeric.negative_p()
         if negative:
             sign = 1
@@ -57,22 +59,28 @@ class GnucashInterface():
         result = copy.to_decimal(None)
         if not result:
             raise Exception("gnc numeric value %s can't be converted to decimal" %
-                            copy.to_string() )
-        digit_tuple = tuple( int(char)
-                             for char in str(copy.num())
-                             if char != '-' )
+                            copy.to_string())
+        digit_tuple = tuple(int(char)
+                            for char in str(copy.num())
+                            if char != '-')
         denominator = copy.denom()
         exponent = int(log10(denominator))
         assert (10 ** exponent) == denominator
-        return float(Decimal( (sign, digit_tuple, -exponent) ))
+        return float(Decimal((sign, digit_tuple, -exponent)))
 
-    """ get_flow """
     def get_flow(self, account):
-        date_start,date_end = self.get_period_limits()
-        return self.gnc_numeric_to_python_Decimal(Account.GetBalanceChangeForPeriod(account,date_start,date_end,True))
+        date_start, date_end = self.get_period_limits()
+        return self.gnc_numeric_to_python_decimal(
+            account.GetBalanceChangeForPeriod(date_start, date_end, True)
+        )
 
     def get_balance(self, account):
-        return self.gnc_numeric_to_python_Decimal(Account.GetBalanceAsOfDateInCurrency(account,self.assess_datetime,self.get_root_commodity(),True))
+        return self.gnc_numeric_to_python_decimal(
+            account.GetBalanceAsOfDateInCurrency(
+                self.assess_datetime,
+                self.get_root_commodity(),
+                True)
+        )
 
     def get_childrenflows(self, account):
         sk_flows = []
@@ -82,28 +90,28 @@ class GnucashInterface():
             total = self.get_flow(acc)
             sk_flows.append(-total)
             acc_names.append(acc.get_full_name())
-        return sk_flows,acc_names
+        return sk_flows, acc_names
 
     def set_deeplevel(self, level):
-        self.deep_leve = level
+        self.deep_level = level
 
-    def get_account_id(self,account):
+    def get_account_id(self, account):
         return self.id_map[account.get_full_name()]
 
     def close_file(self):
         self.session.end()
         self.session.destroy()
 
-    def get_all_accounts(self):
-        incomes = root.lookup_by_name(self.income_name)
-        chlds_inc = incomes.get_children()
-        expenses = root.lookup_by_name(self.expense_name)
-        chlds_exp = expenses.get_children()
-        all_accounts = chlds_inc + chlds_exp
+#    def get_all_accounts(self):
+#        incomes = root.lookup_by_name(self.income_name)
+#        chlds_inc = incomes.get_children()
+#        expenses = root.lookup_by_name(self.expense_name)
+#        chlds_exp = expenses.get_children()
+#        all_accounts = chlds_inc + chlds_exp
 
-    def get_account_level(self,account,level):
+    def get_account_level(self, account, level):
         out_val = []
-        if account == None:
+        if account is None:
             return out_val
         if level == 0:
             return []
@@ -118,11 +126,17 @@ class GnucashInterface():
                     out_val = out_val + out_chld
         return out_val
 
-    def get_expenses(self,depth_level):
-        return self.get_account_level(self.root.lookup_by_name(self.expense_name),depth_level)
+    def get_expenses(self, depth_level):
+        return self.get_account_level(
+            self.root.lookup_by_name(self.expense_name),
+            depth_level
+        )
 
-    def get_income(self,depth_level):
-        return self.get_account_level(self.root.lookup_by_name(self.income_name),depth_level)
+    def get_income(self, depth_level):
+        return self.get_account_level(
+            self.root.lookup_by_name(self.income_name),
+            depth_level
+        )
 
     def get_income_account(self):
         return self.root.lookup_by_name(self.income_name)
@@ -130,18 +144,19 @@ class GnucashInterface():
     def get_expense_account(self):
         return self.root.lookup_by_name(self.expense_name)
 
-    def get_all_accounts(self,depth_level):
-        accounts = self.root.get_descendants()
+    def get_all_accounts(self, base_account, depth_level):
+        base_acc = self.root.lookup_by_name(base_account)
+        accounts = base_acc.get_descendants()
         output = []
         for acc in accounts:
-            if (acc.GetType() != ACCT_TYPE_TRADING and
-                acc.GetType() != ACCT_TYPE_EXPENSE and
-                acc.GetType() != ACCT_TYPE_INCOME  and
-                acc.get_current_depth() <= depth_level) :
-                output.append(acc)       
+            if(acc.GetType() != ACCT_TYPE_TRADING and
+               acc.GetType() != ACCT_TYPE_EXPENSE and
+               acc.GetType() != ACCT_TYPE_INCOME and
+               acc.get_current_depth() <= depth_level):
+                output.append(acc)
         return output
-    
-    def set_assessment_period(self, period:str):
+
+    def set_assessment_period(self, period: str):
         self.assess_period = period
 
     def set_assessment_datetime(self, date_time: str):
@@ -149,21 +164,23 @@ class GnucashInterface():
             self.assess_datetime = datetime.today()
         else:
             parts = date_time.split('.')
-            self.assess_datetime = datetime(int(parts[2]),int(parts[1]),int(parts[0]))
+            self.assess_datetime = datetime(int(parts[2]),
+                                            int(parts[1]),
+                                            int(parts[0]))
 
     def get_period_limits(self):
-        if(self.assess_period == "month"):
-            start = datetime(self.assess_datetime.year,self.assess_datetime.month,1)
-            end = datetime(self.assess_datetime.year,self.assess_datetime.month+1,1)
+        if self.assess_period == "month":
+            start = datetime(self.assess_datetime.year,
+                             self.assess_datetime.month, 1)
+            end = datetime(self.assess_datetime.year,
+                           self.assess_datetime.month+1, 1)
         elif self.assess_period == "year":
-            start = datetime(self.assess_datetime.year,1,1)
-            end = datetime(self.assess_datetime.year+1,1,1)
-        return start,end
+            start = datetime(self.assess_datetime.year, 1, 1)
+            end = datetime(self.assess_datetime.year+1, 1, 1)
+        return start, end
 
     def get_period_string(self):
-        start,end = self.get_period_limits()
-        start_str = start.strftime("%d.%m.%Y") 
+        start, end = self.get_period_limits()
+        start_str = start.strftime("%d.%m.%Y")
         end_str = end.strftime("%d.%m.%Y")
         return start_str + " to " + end_str
-    
-
--- gnucash_sankey.py
+++ gnucash_sankey.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from plotly.offline import plot
 from GnucashInterface import GnucashInterface
 
+
 def process_balances(account_list,gnc_iface):
     flows_list = []
     dest_list = []
@@ -36,22 +37,23 @@ def process_inc_exp(account_list,gnc_iface):
         else:
             origin_list.append(gnc_iface.get_account_id(acc))
             dest_list.append(gnc_iface.get_account_id(acc.get_parent()))
-    return (flows_list,origin_list,dest_list,nodeid_list)
+    return (flows_list, origin_list, dest_list, nodeid_list)
 
-def generate_diagram(origin_list,dest_list,flows_list,names_list,args,gnc_iface):
+
+def generate_diagram(origin_list, dest_list, flows_list, names_list, args, gnc_iface):
     fig = go.Figure(data=[go.Sankey(
             arrangement='snap',
             orientation=args.diag_orientation,
-            node = dict(
-                pad = 10,
-                thickness = 20,
-                line = dict(color = "black", width = 0.5),
-                label = names_list,
+            node=dict(
+                pad=10,
+                thickness=20,
+                line=dict(color="black", width=0.5),
+                label=names_list,
             ),
-            link = dict(
-                source = origin_list, # indices correspond to labels, eg A1, A2, A2, B1, ...
-                target = dest_list,
-                value = flows_list,
+            link=dict(
+                source=origin_list,# indices correspond to labels, eg A1, A2, A2, B1, ...
+                target=dest_list,
+                value=flows_list,
         ))])
 
     title_str = "Gnucash Sankey Diagram " + "[" + args.export_type + "] "
@@ -59,6 +61,7 @@ def generate_diagram(origin_list,dest_list,flows_list,names_list,args,gnc_iface)
     fig.update_layout(title_text=title_str, font_size=10)
     fig.show()
     plot(fig)
+
 
 def main():
     #Parse Arguments
@@ -71,6 +74,8 @@ def main():
     parser.add_argument('--orient', dest='diag_orientation', type=str, default='h',
                     choices=['h','v'],
                     help='diagram oritentation')
+    parser.add_argument('--account', dest='balance_account', type=str, default='Assets',
+                    help='account to diplay balance')
     parser.add_argument('--equityacc', dest='equity_account', type=str, default='Equity',
                     help='equity account name')
     parser.add_argument('--incomeacc', dest='income_account', type=str, default='Income',
@@ -81,37 +86,38 @@ def main():
                     help='time period of assessment')
     parser.add_argument('--date', dest='assess_datetime', type=str, default='current',
                     help='assessment time, in the format dd.mm.yyyy')
-    args = parser.parse_args()    
-    #instatiate the gnucash interface
-    gnc_iface = GnucashInterface(   args.filepath,
-                                    args.income_account,args.expense_account,args.equity_account)
+    args = parser.parse_args()
+    # instatiate the gnucash interface
+    gnc_iface = GnucashInterface(args.filepath,
+                                 args.income_account, args.expense_account, args.equity_account)
 
     gnc_iface.set_assessment_datetime(args.assess_datetime)
     gnc_iface.set_assessment_period(args.assess_period)
-    
-    try: #we use a try/except in case something goes crazy, to still close the gnucash file
+
+    try: # we use a try/except in case something goes crazy, to still close the gnucash file
         if args.export_type == "expenses":
             test = gnc_iface.get_expenses(args.depth_val)
-            (flows_list,origin_list,dest_list,nodeid_list) = process_inc_exp(test,gnc_iface)
+            (flows_list, origin_list, dest_list, nodeid_list) = process_inc_exp(test, gnc_iface)
 
         elif args.export_type == "income":
             test = gnc_iface.get_income(args.depth_val)
-            (flows_list,origin_list,dest_list,nodeid_list) = process_inc_exp(test,gnc_iface)
+            (flows_list, origin_list, dest_list, nodeid_list) = process_inc_exp(test, gnc_iface)
 
         elif args.export_type == "balances":
-            test = gnc_iface.get_all_accounts(args.depth_val)
-            (flows_list,origin_list,dest_list,nodeid_list) = process_balances(test,gnc_iface)
+            test = gnc_iface.get_all_accounts(args.balance_account, args.depth_val)
+            (flows_list, origin_list, dest_list, nodeid_list) = process_balances(test, gnc_iface)
 
         if len(test) > 0:
-            #finally, generate the diagram wil flows, destinations, origins, etc.
-            generate_diagram(origin_list,dest_list,flows_list,gnc_iface.get_account_names(True),args,gnc_iface)
+            # finally, generate the diagram wil flows, destinations, origins, etc.
+            generate_diagram(origin_list, dest_list, flows_list, gnc_iface.get_account_names(True), args, gnc_iface)
         else:
             print('Couldn\'t find any accounts')
     except:
         print("Unexpected error:", sys.exc_info())
 
-    #close gnucash file, even when an error was catched through except.
+    # close gnucash file, even when an error was catched through except.
     gnc_iface.close_file()
+
 
 if __name__ == "__main__":
     main()
